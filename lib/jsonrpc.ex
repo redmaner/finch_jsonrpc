9cf5d5f64d6487f1f692056312444f94002bfb4b
redmaner <jakevdputten@protonmail.com>
2021-08-06 16:01:37 +0200

    Add basic request

--- lib/jsonrpc.ex
+++ lib/jsonrpc.ex
@@ -11,4 +11,27 @@ defmodule Jsonrpc do
   def start_link(opts) do
     Finch.start_link(opts)
   end
+
+  def request(request, options \\ []) do
+    with {name, options} <- options |> Keyword.pop!(:name),
+         {url, options} <- options |> Keyword.pop!(:url),
+         {headers, options} <- options |> Keyword.pop(:headers, []),
+         true <- is_list(headers) and is_binary(url),
+         uri <- URI.parse(url),
+         {:ok, body} <- Jason.encode(request) do
+      %Finch.Request{
+        host: uri.host,
+        path: uri.path,
+        query: uri.query,
+        port: uri.port,
+        scheme: uri.scheme,
+        body: body,
+        headers: headers,
+        method: :post
+      }
+      |> Finch.request(name, options)
+    else
+      _ -> raise "sike"
+    end
+  end
 end
